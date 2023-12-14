9cf9b820eda481174296778a005f03ef8614b7ca
Fiona Mellink <fimellink@gmail.com>
2023-12-14 11:02:37 +1100

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (activity='roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun();
+
+let mondayWork = function(workLocation='go to the office') {
+    return (`This Monday, I will ${workLocation}.`)
+}
+
+function wrapAdjective(empathic) {
+    return function (adjective='special') {
+        return `You are ${empathic}${adjective}${empathic}!`;
+    };
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
